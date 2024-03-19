bd05bf2cac51bd7cebeb01fea9af8bde95a3c7b0
DoanTrongBinh <bdoseasora98@gmail.com>
2024-03-19 10:33:16 +0700

    connect mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 
-app.get('/', (req, res) => res.send('API running'))
+connectDB();
+
+app.get('/', (req, res) => res.send('API running'));
 
 const PORT =  process.env.PORT || 5000;
 
