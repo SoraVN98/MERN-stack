1001d5ab6cd0415265099fcc5227e74c6197797f
DoanTrongBinh <bdoseasora98@gmail.com>
2024-03-19 15:36:55 +0700

    API routes and JWT authentication

--- server.js
+++ server.js
@@ -5,14 +5,17 @@ const app = express();
 // connect DB
 connectDB();
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 // Define routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT =  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,  () => console.log(`Server start on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
