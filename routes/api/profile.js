2abe995197c1ac74c54a21b50eb58024abc1167a
DoanTrongBinh <bdoseasora98@gmail.com>
2024-03-20 14:18:11 +0700

    Get user repo from github

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const request = require('request');
+const config = require('config');
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+
 const { check, validationResult } = require('express-validator');
 
 // @route GET api/profile/me
@@ -231,7 +235,15 @@ router.put(
 
     const { school, degree, fieldofstudy, from, to, current, description } =
       req.body;
-    const newEdu = { school, degree, fieldofstudy, from, to, current, description };
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
 
     try {
       const profile = await Profile.findOne({ user: req.user.id });
@@ -258,5 +270,37 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   await profile.save();
 
   res.json(profile);
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repo from github
+// @access Public
+router.get('/github/:username', (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?
+        per_page=5
+        &sort=created:asc
+        &client_id=${config.get('githubClientId')}
+        &client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) {
+        console.log(error.message);
+      }
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: 'No github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 module.exports = router;
